3824a0655e80cb00540072e73196bb6fe299268b
icapistrano <cc.igor@gmail.com>
2018-01-01 14:34:22 -0300

    Added routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,9 +1,25 @@
 // let's go!
 import React from 'react'
-
 import { render } from 'react-dom'
 
 //import css
 import css from './styles/style.styl'
 
-render(<p>gi</p>, document.getElementById('root'))
+//import components
+import Main from './components/Main'
+import Single from './components/Single'
+import Photogrid from './components/Photogrid'
+
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory} from 'react-router'
+
+const router = (
+    <Router history={browserHistory}> 
+        <Route path='/' component={Main}>
+            <IndexRoute component={Photogrid}></IndexRoute>
+            <Route path='/view/:postId' component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'))
